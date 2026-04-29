f16f16bcbdee6c0cb52a85fb53edb4bba5746efb
harold ponce <harold.andres.ponce@gmail.com>
2026-04-29 17:07:49 +0200

    delete de usuarios

---
+++ src/actions/user_delete.proc.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include_once __DIR__ . '/../config/paths.php';
+include_once PATH_BASE . 'includes/db_connect.php';
+/*if(!isset($_SESSION['autenticated']) || $_SESSION['autenticated'] !== true){
+    header('Location:' . URL_BASE . 'views/login.php');
+    exit;
+}*/
+
+if(isset($_REQUEST["id_user"])){
+    $id = $_REQUEST["id_user"];
+
+    $stmt=$db->prepare('DELETE FROM users WHERE usu_id = :id_usu');
+    $stmt->bindValue(':id_usu', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    header('Location:' . URL_BASE . 'views/panelAdmin.php');
+    exit;
+
+}else{
+    echo "ni veo el id";
+}
+
+?>
